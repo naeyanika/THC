b3fea4e28eef6933b64dd4d3ea88237eb9d26ee2
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-07-25 10:14:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import io
 
 st.title('Aplikasi Pengolahan THC')
 st.write("""1. Format file harus bernama dan menggunakan ekstensi csv, sehingga seperti ini : THC.csv, DbPinjaman.csv, DbSimpanan.csv.""")
-st.write("""2. File nya di rapikan header dan footer nya.""")
+st.write("""2. File THC di rapikan header dan footer nya sperti pengolahan biasa, dan untuk kolom Debit dan Credit dibiarkan ada 2 dan jangan dihapus!.""")
+st.write("""3. File DbSimpanan dan DbPinjaman, hapus header nya saja.""")
 st.write("""3. Jika penjelasan diatas kurang paham, kalian bisa lihat contohnya link dibawah ini : https://bit.ly/contoh-data-thc""")
 
 # Function to format numbers
