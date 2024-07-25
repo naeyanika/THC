17bedbbb9f3b18c14a715ab9fa6b9414bb8cacc7
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-07-25 10:11:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import io
 
 st.title('Aplikasi Pengolahan THC')
-st.write("""1. Format file harus bernama dan menggunakan ekstensi csv, sehingga seperti ini : THC.csv, DbPinjaman.csv, DbSimpanan.csv.*""")
-st.write("""2. File nya di rapikan header dan footer nya.*""")
+st.write("""1. Format file harus bernama dan menggunakan ekstensi csv, sehingga seperti ini : THC.csv, DbPinjaman.csv, DbSimpanan.csv.""")
+st.write("""2. File nya di rapikan header dan footer nya.""")
 st.write("""3. ika penjelasan diatas kurang paham, kalian bisa lihat contohnya link dibawah ini : https://bit.ly/contoh-data-thc""")
 
 # Function to format numbers
