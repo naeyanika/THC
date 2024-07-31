e25f33ce4d2761590c620683dd3590ae063a63c8
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-07-31 09:40:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,13 +90,14 @@ if uploaded_files:
         
         df3['TRANS. DATE'] = pd.to_datetime(df3['TRANS. DATE'], format='%d/%m/%Y', errors='coerce')
         df3['ENTRY DATE'] = pd.to_datetime(df3['ENTRY DATE'], format='%d/%m/%Y', errors='coerce')
-        
-        st.write("THC setelah diproses:")
-        st.write(df3)
-        
+                
         # Filter N/A
         df3_na = df3.dropna(subset=['DOCUMENT NO.'])
-        df3_blank = df3_na[df3_na['DOCUMENT NO.'].str.startswith('None')].copy()
+        df3_blank = df3_na[df3_na['DOCUMENT NO.'].str.startswith('')].copy()
+
+        st.write("THC setelah diproses:")
+        st.write(df3)
+
     
         st.write("THC DOC Blank:")
         st.write(df3_na)
